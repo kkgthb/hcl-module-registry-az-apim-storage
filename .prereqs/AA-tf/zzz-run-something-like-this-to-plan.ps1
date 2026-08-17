18f611ea32d134e36fb3366fda2c01f3a0ee88ad
kkgthb <44843658+kkgthb@users.noreply.github.com>
2026-08-17 11:51:26 -0500

    Manual workload rename in PS since doing 2 at a time today

--- .prereqs/AA-tf/zzz-run-something-like-this-to-plan.ps1
+++ .prereqs/AA-tf/zzz-run-something-like-this-to-plan.ps1
@@ -9,6 +9,6 @@ terraform init
 terraform plan `
     -var entra_tenant_id="$([Environment]::GetEnvironmentVariable('DEMOS_my_entra_tenant_id', 'User'))" `
     -var az_sub_id="$([Environment]::GetEnvironmentVariable('DEMOS_my_azure_subscription_id', 'User'))" `
-    -var workload_nickname="$([Environment]::GetEnvironmentVariable('DEMOS_my_workload_nickname', 'User'))" 
+    -var workload_nickname="$([Environment]::GetEnvironmentVariable('DEMOS_my_workload_nickname', 'User'))-hclreg" 
 
 Pop-Location
